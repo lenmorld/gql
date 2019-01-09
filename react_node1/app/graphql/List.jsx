4f3c6861ce29be97043dc0f4de8f6e0bd3ef81ec
lenny <lenmorld@gmail.com>
2019-01-08 22:41:14 -0500

    4_F_apollo react query Object type

--- react_node1/app/graphql/List.jsx
+++ react_node1/app/graphql/List.jsx
@@ -4,24 +4,54 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const List = () => (
-    <Query
-        query={gql`
-            {
-                titles
+    <div>
+        <Query
+            query={gql`
+                {
+                    titles
+                }
+            `}
+        >
+            {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>;
+                if (error) return <p>Error :( </p>;
+                console.log(data);
+                return data.titles.map((item, index) => (
+                    <div key={index}>
+                        <p>{item}</p>
+                    </div>
+                ));
+            }}
+        </Query>
+
+        <hr />
+
+        <Query
+            query={gql`
+                {
+                    list {
+                        id
+                        artist
+                        album
+                        title
+                    }
+                }
+            `
             }
-        `}
-    >
-        {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error :( </p>;
-            console.log(data);
-            return data.titles.map((item, index) => (
-                <div key={index}>
-                    <p>{item}</p>
-                </div>
-            ));
-        }}
-    </Query>
+        >
+           {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>;
+                if (error) return <p>Error :( </p>;
+                console.log(data);
+                return data.list.map(({id, artist, album, title}) => (
+                    <div key={id}>
+                        <p>{artist} - {album} - {title}</p>
+                    </div>
+                ));
+            }}
+        </Query>
+    </div>
+
 );
 
 export default List;
